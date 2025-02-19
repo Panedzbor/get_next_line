65fc3307fe4863c91f1bdcadf4cd1ca593ce8562
Edgar Arutiunian <panedzbor@gmail.com>
2025-02-19 20:32:56 +0100

    raw

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,154 @@
+#include "get_next_line.h"
+
+#define BUFFER_SIZE 42
+
+char    *get_next_line(int fd)
+{
+    ssize_t     len;
+    char        buffer[BUFFER_SIZE + 1];
+    char        *next_line;
+    static t_st st;
+    bool        buf;
+
+    if (!st.left)
+        init_struct(&st, fd);
+    buf = false;
+    if (!st.left || st.check != 0)
+    {
+        len = read(fd, buffer, BUFFER_SIZE);
+        if (len == -1)
+            return (NULL);
+        buffer[len] = '\0';
+        buf = true;
+    }
+    else
+        len = count_size(st.left);
+    if (buf)
+        next_line = search_newl(buffer, len, &st, buf);
+    else
+        next_line = search_newl(st.left, len, &st, buf);
+    return (next_line);
+}
+
+void    init_struct(t_st *st, int fd)
+{
+    st->left = NULL;
+    st->check = 0;
+    st->fd = fd;
+}
+
+ssize_t count_size(char *str)
+{
+    ssize_t i;
+
+    i = 0;
+    while (str[i] != '\0')
+        i++;
+    return (i - 1);
+}
+
+char *search_newl(char *str, ssize_t len, t_st *st, bool *buf)
+{
+    ssize_t  i;
+    char    *next_line;
+
+    i = 0;
+    while (i < len)
+    {
+        if (str[i] == '\n')
+        {
+            if (!buf)
+                st->check = 1;
+            next_line = compose_newl(str, i, st);
+            st->left = overwrite_left(len, str, i, st);
+            return (next_line);
+        }
+        i++;
+    }
+    if (!buf)
+        st->check = -1;
+    else
+        st->left = concat_left(len, str, st);
+    next_line = get_next_line(t_st->fd);
+    return (next_line);
+}
+
+char *compose_newl(char *str, ssize_t last_char, t_st *st)
+{
+    char    *next_line;
+    ssize_t i;
+    ssize_t leftlen;
+    
+    leftlen = 0;
+    if (st->left && st->check == -1)
+        leftlen = count_size(st->left);
+    next_line = (char *)malloc(leftlen + last_char + 2);
+    if (!next_line)
+        return (NULL);
+    i = 0;
+    while (i < leftlen)
+    {
+        next_line[i] = st->left[i];
+        i++;
+    }
+    if (leftlen > 0)
+        st->left = overwrite_left(0, st->left, 0, st);
+    while (i - leftlen <= last_char)
+    {
+        next_line[i] = str[i];
+        i++;
+    }
+    next_line[i] = '\0';
+    return (next_line);
+}
+
+char *overwrite_left(ssize_t len, char *str, ssize_t i, t_st *st)
+{
+    char    *new_left;
+    ssize_t y;
+    
+    if (len - i <= 1)
+        return (erase_left(st));
+    new_left = (char *)malloc(len - i);
+    if (!new_left)
+        return (NULL);
+    i++;
+    y = 0;
+    while (i < len + 1)
+        new_left[y++] = str[i++];
+    erase_left(st);
+    return (new_left);
+}
+
+
+char    *erase_left(t_st *st)
+{
+    if (st->left)
+        free((void *)st->left);
+    st->check = 0;
+    return (NULL);   
+}
+
+char *concat_left(ssize_t len, char *str, t_st *st)
+{
+    char    *new_left;
+    ssize_t     i;
+    ssize_t     j;
+
+    len += count_size(st->left);
+    new_left = (char *)malloc(len);
+    if (!new_left)
+        return (NULL);
+    i = 0;
+    while (st->left[i] != '\0')
+    {
+        new_left[i] = st->left[i];
+        i++;
+    }
+    i--;
+    j = 0;
+    while (j <= len)
+        new_left[i++] = str[j++];
+    erase_left(st);
+    return (new_left);
+}
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: earutiun <earutiun@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 14:22:47 by earutiun          #+#    #+#             */
+/*   Updated: 2025/02/18 14:23:51 by earutiun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_statvals
+{
+    char    *left;
+    int     check;
+    int     fd;
+} t_st;
+
+char    *get_next_line(int fd);
+
+#endif
